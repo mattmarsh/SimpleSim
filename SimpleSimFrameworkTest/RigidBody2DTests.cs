1511e4da88c000aeec714908c22903dfb3e7f9a7
Matt Marsh <matt@mmarsh.net>
2019-06-29 19:36:59 -0600

    add these unit tests

---
+++ SimpleSimFrameworkTest/RigidBody2DTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSimFramework.Models;
+using SimpleSimFramework;
+namespace SimpleSimFrameworkTest
+{
+    [TestClass]
+    public class RigidBody2DTests
+    {
+        [TestMethod]
+        public void TestForceNoTorqueNoGrav()
+        {
+            MassProperties mp = new MassProperties(10, 1);
+            RigidBody2D m = new RigidBody2D(mp);
+            Guid id = Guid.NewGuid();
+            m.SetForce(id, new Vector2D(1, 2), Vector2D.Zero);
+            var onesec = TimeSpan.FromSeconds(1);
+            var tol = 0.001;
+
+            m.Run(onesec);
+            Assert.AreEqual(0.1, m.Velocity.x, tol);
+            Assert.AreEqual(0, m.Position.x, tol);
+            Assert.AreEqual(0.2, m.Velocity.y, tol);
+            Assert.AreEqual(0, m.Position.y, tol);
+
+            m.Run(onesec);
+            Assert.AreEqual(0.2, m.Velocity.x, tol);
+            Assert.AreEqual(0.1, m.Position.x, tol);
+            Assert.AreEqual(0.4, m.Velocity.y, tol);
+            Assert.AreEqual(0.2, m.Position.y, tol);
+
+            m.SetForce(id, Vector2D.Zero, Vector2D.Zero);
+            m.Run(onesec);
+            Assert.AreEqual(0.2, m.Velocity.x, tol);
+            Assert.AreEqual(0.3, m.Position.x, tol);
+            Assert.AreEqual(0.4, m.Velocity.y, tol);
+            Assert.AreEqual(0.6, m.Position.y, tol);
+        }
+
+        [TestMethod]
+        public void TestForceTorqueNoGrav()
+        {
+            MassProperties mp = new MassProperties(10, 10);
+            RigidBody2D m = new RigidBody2D(mp);
+            Guid id1 = Guid.NewGuid();
+            Guid id2 = Guid.NewGuid();
+
+            // Add two opposing forces on opp sides of moment arm
+            m.SetForce(id1, new Vector2D(0, 1), new Vector2D(1, 0));
+            m.SetForce(id2, new Vector2D(0, -1), new Vector2D(-1, 0));
+
+            var onesec = TimeSpan.FromSeconds(1);
+            var tol = 0.001;
+
+            m.Run(onesec);
+            Assert.AreEqual(0.2, m.AngularVelocity, tol);
+            Assert.AreEqual(0.0, m.Orientation, tol);
+            m.Run(onesec);
+            Assert.AreEqual(0.4, m.AngularVelocity, tol);
+            Assert.AreEqual(0.2, m.Orientation, tol);
+
+            // remove forces
+            m.SetForce(id1, Vector2D.Zero, Vector2D.Zero);
+            m.SetForce(id2, Vector2D.Zero, Vector2D.Zero);
+            m.Run(onesec);
+            Assert.AreEqual(0.4, m.AngularVelocity, tol);
+            Assert.AreEqual(0.6, m.Orientation, tol);
+        }
+
+        [TestMethod]
+        public void TestGrav()
+        {
+            MassProperties mp = new MassProperties(10, 1);
+            RigidBody2D m = new RigidBody2D(mp);
+            m.Gravity = new SimpleGravity(new Vector2D(0, -1));
+
+            var onesec = TimeSpan.FromSeconds(1);
+            m.Run(onesec);
+            Assert.AreEqual(-1, m.Velocity.y, 0.001);
+            Assert.AreEqual(0, m.Position.y, 0.001);
+            m.Run(onesec);
+            Assert.AreEqual(-2, m.Velocity.y, 0.001);
+            Assert.AreEqual(-1, m.Position.y, 0.001);
+            m.Run(onesec);
+            Assert.AreEqual(-3, m.Velocity.y, 0.001);
+            Assert.AreEqual(-3, m.Position.y, 0.001);
+            Assert.AreEqual(0, m.Velocity.x, 0.001);
+            Assert.AreEqual(0, m.Position.x, 0.001);
+        }
+    }
+}
---
+++ SimpleSimFrameworkTest/UtilTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSimFramework;
+
+namespace SimpleSimFrameworkTest
+{
+    [TestClass]
+    public class UtilTests
+    {
+        [TestMethod]
+        public void TestTo360()
+        {
+            var tol = 0.001;
+            Assert.AreEqual(359, Util.To360(-1), tol);
+            Assert.AreEqual(10, Util.To360(10 - 360), tol);
+            Assert.AreEqual(359, Util.To360(359), tol);
+            Assert.AreEqual(10, Util.To360(10 - 360*2), tol);
+            Assert.AreEqual(182, Util.To360(182 + 360), tol);
+            Assert.AreEqual(182, Util.To360(182 + 360*2), tol);
+        }
+
+        [TestMethod]
+        public void TestTo2PI()
+        {
+            var tol = 0.001;
+            Assert.AreEqual(6, Util.To2Pi(6), tol);
+            Assert.AreEqual(1, Util.To2Pi(1), tol);
+            Assert.AreEqual(6, Util.To2Pi(6 - 2*Math.PI), tol);
+            Assert.AreEqual(6, Util.To2Pi(6 - 4 * Math.PI), tol);
+            Assert.AreEqual(6, Util.To2Pi(6 + 4 * Math.PI), tol);
+        }
+
+        [TestMethod]
+        public void TestPi2NegPi()
+        {
+            var tol = 0.001;
+            Assert.AreEqual(6 - Math.PI, Util.ToPiNegPi(6), tol);
+        }
+    }
+}
